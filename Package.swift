139b8f16c88e27f23829ca12c8e7dda3e3081ece
huyan_18040019 <huyan@shinhofood.com>
2019-10-24 14:10:49 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -24,7 +24,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SPM_OCTest",
-            path: "Sources"),
+            path: "Sources",
+            publicHeadersPath: "../Sources"
+        ),
         .testTarget(
             name: "SPM_OCTestTests",
             dependencies: ["SPM_OCTest"]),
--- Sources/Test2.m
+++ Sources/Test2.m
@@ -1,14 +1,14 @@
 //
-//  Test.m
+//  Test2.m
 //  SPM_OCTest
 //
 //  Created by Yan Hu on 2019/10/24.
 //  Copyright © 2019 yan. All rights reserved.
 //
 
-#import "Test.h"
+#import "Test2.h"
 
-@implementation Test
+@implementation Test2
 
 - (void)testRun {
     NSLog(@"%s", "test run");
